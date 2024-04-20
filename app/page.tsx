ed94d0f7e9139c8d7c401295e31428213faf393f
Adam Hudak <adamhudak1@proton.me>
2024-04-19 21:12:52 -0500

    feat: Live cursors

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
+"use client";
+
+import Live from "@/components/Live";
+
 export default function Page() {
-  return (
-    <h1 className="text-2xl text-white">
-      <div className="h-[100vh] w-full flex justify-center items-center text-center">
-        Figma Room
-      </div>
-    </h1>
-  );
+  return <Live />;
 }
